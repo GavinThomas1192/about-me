9c7bd1b396c4503d6ac2fee28a34c020b7d8d462
GavinThomas1192 <gthomas1192@gmail.com>
2017-06-07 16:19:47 -0700

    Added question 6 and 7

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var askQuestion = confirm('Hello, I\'m Gavin. Want to play a guessing game?');
 if (askQuestion === true) {
   alert('Great! I\'m Gavin here are some questions to guess!');
+  //Question 1
   var gender = prompt('Since you already know my name, do you think I am a boy or girl?');
 
   if (gender.toLowerCase() === 'boy') {
@@ -11,30 +12,68 @@ if (askQuestion === true) {
     alert('Wrong guess!');
 
   }
+  //Question 2
   var age = prompt('Now you know my name is Gavin and I\'m a boy. Do you think I am less than 20 years old?');
   if (age.toLowerCase() == 'no' || age.toLowerCase() == 'n') {
     alert('Wow, you\'re on top of it! I am older than 20.');
   } else {
     alert('Whoops wrong!');
   }
-  var exactAge = prompt('Now that you know I\'m older than 20. I want you to guess my exact age. It\'s a number between 23 and 25');
-  if (exactAge == 24) {
+  //Question 3
+  var exactAge = prompt('Now that you know I\'m older than 20. I want you to guess my exact age. It\'s a number between 23 25.');
+  if (exactAge === 24) {
     alert('Man, you might as well go buy some lotto tickets, you\'re on fire!');
   } else {
     alert('How in the world did you get that wrong?');
- }
+  }
+  //Question 4
   var myKids = prompt('I\'m glad you are getting to know me. How many children do you think I have?');
   if (myKids === '1') {
     alert('I do have one child, and he is amazing.');
   } else {
     alert('Error, you guessed wrong!');
   }
+  //Question 5
   var currentCity = prompt('Last question, what city do I live in? (Tacoma, Seattle, Sumner, or Bellevue)');
-  if (currentCity.toLowerCase() === "tacoma") {
+  if (currentCity.toLowerCase() === 'tacoma') {
     alert('Wow you nailed it again! Now go buy me some lottery tickets!');
   } else {
     alert('Wrong');
   }
+  //random number question 6 !!!!Help
+  var totalTries = 4;
+  var randomNumber = Math.floor(Math.random() * 10 + 1);
+  while (totalTries > 0) {
+    var randomNumberAns = prompt('I\'m thinking of a whole random number between 1 and 10. Can you guess it with only 4 tries?');
+    if (randomNumberAns == randomNumber) {
+      alert('Wow! You guessed it!');
+      totalTries = 0;
+    } else {
+      alert('Oh no! Guess again!');
+      totalTries--;
+    }
+  }
+
+  //question number 7-Mult correct anwsers
+  var questionSevenTries = 6;
+  var citiesLived = ['lakewood', 'tacoma', 'steilacoom', 'auburn', 'Seattle', 'des moines', 'sea-tac'];
+  var isRight = false;
+  while (questionSevenTries > 0) {
+    var whichCity = prompt('I have lived in three out of these seven cities. Can you guess at least one? (Tacoma, Lakewood, Steilacoom, Auburn, Seattle, Des Moines, Sea-Tac,)');
+
+    for (var i = 0; i < citiesLived.length; i++) {
+      if (citiesLived[i] == whichCity.toLowerCase()) {
+        alert('You guessed right!');
+        questionSevenTries = 0;
+        isRight = true;
+      }
+    }
+    if (isRight === false) {
+      alert('wrong');
+      questionSevenTries--;
+    }
+  }
+
 } else {
   alert('Okay, play again soon!');
 }
